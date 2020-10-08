23fe0b3509e7d3fef98b79f7230ce8ddc9fc4850
Luis Caroca <kako.ljca@gmail.com>
2020-10-07 21:39:18 -0300

    Primer commit, agregando primera vista sin terminar)

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Inicio</title>
+        <meta charset="UTF-8">
+        <link href="public_html/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="public_html/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <header class="superior">
+            <div class="logo">
+                <img src="public_html/images/Logo transparente.png">
+            </div>
+            <nav>
+                <ul class="menu">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="#">Contenido</a></li>
+                    <li><a href="#">Agenda tu hora</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+        <article>
+            <img id="imgpsicologa" src="public_html/images/Psicologa.jpg">
+            <h1 class="titulo">Sobre mí:</h1>
+            </br>
+            <h4 class="texto">
+                Decidir comenzar un proceso de terapia no es fácil, pero a la vez es una de las decisiones más 
+                importantes que se pueden tomar por uno mismo. Implica mirarse y entrar en aspectos que por lo 
+                general evitamos hasta que nos empieza a generar malestar, reclamando nuestra atención. Pero con 
+                un buen acompañamiento, este proceso logra tener frutos que resultan ser maravillosos para nuestras vidas.
+                </br>
+                </br>
+                Si en este momento hay algún aspecto de tu vida que quisieras mejorar, algo que sientas que te está aproblemando, que 
+                ya te está generando agotamiento y una sensación de que tienes que "lidiar" con eso, sin poder fluir como quisieras, 
+                tal vez sería bueno considerar el tiempo de iniciar una terapia. Si tienes dudas acerca de si puedo ayudarte, estaré 
+                feliz de que me contactes y me cuentes más acerca de tu problema.
+                </br>
+                </br>
+                María José Caroca
+            </h4>
+            </br>
+            </br>
+            <h1>Servicios:</h1>
+            </br>
+            <h4>
+                •Psicología Clínica.
+                </br>
+                •Psicología Clínica de Adultos.
+                </br>
+                •Psicoterapia.
+                </br>
+                •Psicoterapia en adolescentes y adultos.
+            </h4>
+            </br>
+            <h1>Enfermedades tratadas:</h1>
+            <h4>
+                •Estrés.
+                </br>
+                •Violencia intrafamiliar.
+                </br>
+                •Trastorno de estrés postraumático.
+                </br>
+                •Trastornos del ánimo.
+                </br>
+                •Maltrato grave.
+                </br>
+                •Trastorno de control de impulsos.
+                </br>
+                •Problemas de autoestima.
+                </br>
+                •Depresión.
+                </br>
+                •Duelos Patológicos.
+                </br>
+                •Trastornos adaptativos.
+                </br>
+                •Trastornos de ansiedad.
+            </h4>
+        </article>                
+    </body>
+</html>
