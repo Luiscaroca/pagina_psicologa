31593709bfe42e3498d117d3f27a58936cc6a784
Luis Caroca <kako.ljca@gmail.com>
2020-10-08 15:39:04 -0300

    Se agregaron las vistas (vacias aun) en conjunto con sus direccionamientos y se añadio el favicon para el sitio

---
+++ contenido.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Inicio</title>
+        <meta charset="UTF-8">
+        <link rel="icon" href="public_html/images/favicon.ico" type="image/x-icon"/>
+        <link href="public_html/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="public_html/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <header class="superior">
+            <div class="logo">
+                <img src="public_html/images/Logo transparente.png">
+            </div>
+            <nav>
+                <ul class="menu">
+                    <li><a href="inicio.php">Inicio</a></li>
+                    <li><a href="contenido.php">Contenido</a></li>
+                    <li><a href="agendar_hora.php">Agenda tu hora</a></li>
+                    <li><a href="contacto.php">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+        </br>
+        <h1>Contenido:</h1>              
+    </body>
+</html>
--- inicio.php
+++ inicio.php
@@ -4,6 +4,7 @@
     <head>
         <title>Inicio</title>
         <meta charset="UTF-8">
+        <link rel="icon" href="public_html/images/favicon.ico" type="image/x-icon"/>
         <link href="public_html/css/styles.css" rel="stylesheet" type="text/css"/>
         <script src="public_html/js/jquery-3.5.1.min.js" type="text/javascript"></script>
     </head>
@@ -14,13 +15,14 @@
             </div>
             <nav>
                 <ul class="menu">
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="#">Contenido</a></li>
-                    <li><a href="#">Agenda tu hora</a></li>
-                    <li><a href="#">Contacto</a></li>
+                    <li><a href="inicio.php">Inicio</a></li>
+                    <li><a href="contenido.php">Contenido</a></li>
+                    <li><a href="agendar_hora.php">Agenda tu hora</a></li>
+                    <li><a href="contacto.php">Contacto</a></li>
                 </ul>
             </nav>
         </header>
+        </br>
         <article>
             <img id="imgpsicologa" src="public_html/images/Psicologa.jpg">
             <h1 class="titulo">Sobre mí:</h1>
