8b9d1652a06665098906994b22df5f8b7ab8c356
Luis Caroca <kako.ljca@gmail.com>
2020-11-25 22:41:37 -0300

    Se modificó vista contacto

--- public_html/contacto.php
+++ public_html/contacto.php
@@ -65,33 +65,49 @@
         <main>
             <section class="popular-places">
                 <div class="container">
-                    <h3>Puedes contactarme directamente:</h3>
-                    </br>
 
-                    <img src="img/correo.png" alt=""/>
-                    <a href="mailto:mjcaroca.araya@gmail.com">Da clic aquí para enviarme un correo</a>
-
-                    </br>
-                    </br>
-
-                    <img src="img/whatsApp.png" alt=""/>
-                    <a href="https://api.whatsapp.com/send?phone=56933914571">Da clic aquí para comunicarnos vía WhatsApp</a>
-
-                    </br>
-                    <h3>O puedes ver mis datos de contacto aquí:</h3>
-                    </br>
-
-                    <div class="float">
-                        <img src="img/correo.png" alt=""/>
+                    <div class="col-md-6">
+                        <div class="left-content">
+                            <h3><strong>Puedes contactarme directamente:</strong></h3>
+                            </br>
+                            <form action="mailto:mjcaroca.araya@gmail.com?subject=Contacto vía página web" method="post" enctype="text/plain">
+                                <li>
+                                    <label>Nombre:</label>
+                                    <input type="text" name="Nombre" id="textnombre">
+                                </li>
+                                </br>
+                                <li>
+                                    <label id="labelcomentario">Deja tu comentario:</label>
+                                    <textarea name="Comentario" id="textcomentario"></textarea>
+                                </li>
+                                </br>
+                                <input type="submit" value="Enviar" class="modal-btn" style="margin-left: 29%;">
+                            </form>
+                        </div>
                     </div>
-                    <h5 class="text-contact"><strong>Correo:</strong> mjcaroca.araya@gmail.com</h5>
 
+                    <div class="col-md-6">
+                        <div class="right-content">
+                            <h3><strong>También puedes ver mis datos de contacto:</strong></h3>
+                            </br>
+                            <div class="float">
+                                <img src="img/correo.png" alt=""/>
+                            </div>
+                            <h5 class="text-contact"><strong>Correo:</strong> mjcaroca.araya@gmail.com</h5>
+
+                            </br>
+
+                            <div class="float">
+                                <img src="img/telefono.png" alt=""/>
+                            </div>
+                            <h5 class="text-contact"><strong>Número de teléfono:</strong> +56 9 3391 4571</h5>
+                            </br>
+                            <a href="https://api.whatsapp.com/send?phone=56933914571" class="fa fa-whatsapp"> Da clic aquí para comunicarnos vía WhatsApp directamente.</a>
+                        </div>
+                    </div>
                     </br>
 
-                    <div class="float">
-                        <img src="img/telefono.png" alt=""/>
-                    </div>
-                    <h5 class="text-contact"><strong>Número de teléfono:</strong> +56 9 3391 4571</h5>
+
 
                 </div>
             </section>
